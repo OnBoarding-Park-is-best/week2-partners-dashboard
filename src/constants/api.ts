cb72eb2b4e573184dceb574cd459591c70ef2212
hyo-choi <hyochoi31@gmail.com>
2022-02-08 20:23:53 +0900

    :package: chore: 배포용 설정 수정

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-const { REACT_APP_API_HOST, REACT_APP_API_PORT } = process.env;
+const { REACT_APP_API_URL } = process.env;
 
-export const API_URL = `${REACT_APP_API_HOST}:${REACT_APP_API_PORT}/requests`;
+export const API_URL = `${REACT_APP_API_URL}/requests`;
